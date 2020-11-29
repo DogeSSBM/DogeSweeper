ebf82f85e2b04a11b1b906508a13044f0ff09ab8
dogeThinkpad <dogessbm@gmail.com>
2020-11-29 18:11:21 -0500

    Automatic commit.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include "DogeLib/Includes.h"
 
+typedef struct{
+	bool isBomb;
+	bool hasFlag;
+	bool isCleared;
+	int num;
+}Tile;
+
+typedef struct{
+	Length dimension;
+	uint numBombs;
+	Tile **tile;
+}Board;
+
+Board* newBoard(const Length len, const uint numBombs)
+{
+	
+}
+
 int main(int argc, char const *argv[])
 {
 	const Length window = {800, 600};
